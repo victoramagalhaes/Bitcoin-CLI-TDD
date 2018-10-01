89e8bbd9591c770f45323934af6f2a17594f5ea6
Victor Magalhães <victor@divweb.com.br>
2018-10-01 01:41:01 -0300

    Install commander and making it works

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,15 +1,15 @@
 const expect = require('chai').expect;
 
 const exec = require('child_process').exec;
-const btcConverter = './src/main.js'
+const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
 describe('Main CLI', () => {
-    it('should return Hello World', (done) => {
-        exec(btcConverter, (err, stdout, stderr) => {
+    it('should return version of BTC-CLI', (done) => {
+        exec(`${btcConverter} --version`, (err, stdout, stderr) => {
             if(err) throw err;
-            expect(stdout.replace('\n','')).to.be.equal('Hello World!');
+            expect(stdout.replace('\n','')).to.be.equal(pkg.version);
             done();
         });
-        
     });
 });
