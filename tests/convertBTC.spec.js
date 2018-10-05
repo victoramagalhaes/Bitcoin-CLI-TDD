92aa9a2aa64edb45f324ab19ce0b4b1c387f795a
Victor Magalhães <victor@divweb.com.br>
2018-10-05 17:32:25 -0300

    Implement call to API

--- tests/convertBTC.spec.js
+++ tests/convertBTC.spec.js
@@ -1,13 +1,42 @@
 const chai = require('chai');
 const expect = chai.expect;
+const nock = require('nock');
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai);
 
 const convertBTC = require('../src/convertBTC.js');
 
 describe('ConvertBTC', () => {
-     it('should return USD as currency and 1 as amount default', () => {
-          expect(convertBTC()).to.be.equal('1 BTC to USD = 2000.00');
-     });
-     it('should return BRL as currency and 10 as amount when defined', () => {
-          expect(convertBTC('BRL',10)).to.be.equal('10 BTC to BRL = 2000.00');
+     
+     let consoleStub;
+     
+     const responseMock = {
+          "time": "2018-10-05 19:42:16",
+          "price": 6575.31,
+          "success": true
+     }
+
+     beforeEach(() => {
+          consoleStub = sinon.stub(console,'log');
+     })
+     afterEach(() => {
+          console.log.restore();
+     })
+
+     it('should return USD as currency and 10 as amount default', (done) => {
+          // https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=1
+          nock('https://apiv2.bitcoinaverage.com')
+               .get('/convert/global')
+               .query({ from: 'BTC', to: 'USD', amount: 10 })
+               .reply(200, responseMock);
+          
+          convertBTC('USD', 10);
+
+          setTimeout(() => {
+               expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 6575.31');
+               done();
+          }, 300)
      });
 });
