f3b7c63e023af13126e24731d14c6965660b1c4c
Victor Magalhães <victor@divweb.com.br>
2018-10-09 22:19:01 -0300

    Get with the API the real data. Finish the first version.

--- tests/convertBTC.spec.js
+++ tests/convertBTC.spec.js
@@ -17,6 +17,11 @@ describe('ConvertBTC', () => {
           "price": 6575.31,
           "success": true
      }
+     const responseMockBRL = {
+          "time": "2018-10-10 00:14:23",
+          "price": 24632.48,
+          "success": true
+     }
 
      beforeEach(() => {
           consoleStub = sinon.stub(console,'log');
@@ -31,12 +36,39 @@ describe('ConvertBTC', () => {
                .get('/convert/global')
                .query({ from: 'BTC', to: 'USD', amount: 10 })
                .reply(200, responseMock);
-          
+           
           convertBTC('USD', 10);
 
           setTimeout(() => {
                expect(consoleStub).to.have.been.calledWith('10 BTC to USD = 6575.31');
                done();
-          }, 300)
+          }, 300);
+     });
+     it('should return BRL as currency and 1 as amount default', (done) => {
+          // https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=BRL&amount=1
+          nock('https://apiv2.bitcoinaverage.com')
+               .get('/convert/global')
+               .query({ from: 'BTC', to: 'BRL', amount: 1 })
+               .reply(200, responseMockBRL);
+           
+          convertBTC('BRL');
+
+          setTimeout(() => {
+               expect(consoleStub).to.have.been.calledWith('1 BTC to BRL = 24632.48');
+               done();
+          }, 300);
+     });
+     it('should message user when api reply with error', (done) => {
+          nock('https://apiv2.bitcoinaverage.com')
+               .get('/convert/global')
+               .query({ from: 'BTC', to: 'BRL', amount: 1 })
+               .replyWithError('Error');
+           
+          convertBTC('BRL');
+
+          setTimeout(() => {
+               expect(consoleStub).to.have.been.calledWith('Something went wrong in the API. Try in a few minutes.');
+               done();
+          }, 300);
      });
 });
